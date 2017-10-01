cbe756c6fcb85a20303cfdc76a372e8f58479bed
boris.d <boris.dutkin@gmail.com>
2017-10-01 13:18:50 +0300

    add customer creation and show pages

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,6 +6,39 @@
  */
 
 module.exports = {
-	
+
+  new: function(request, response) {
+    response.view()
+  },
+
+  create: function(request, response, next) {
+    Customer.create(request.params.all(), function customerCreated(error, customer) {
+
+      // error
+      if(error) return next(error);
+
+      // redirect to show page
+      response.redirect('/customer/show/' + customer.id);
+
+    });
+  },
+
+  show: function(request, response, next) {
+    Customer.findOne(request.param('id'), function customerCreated(error, customer) {
+
+      // error
+      if(error) return next(error);
+
+      // no customer
+      if(!customer) return next();
+
+      // return view with customer
+      response.view({
+        customer: customer
+      });
+
+    });
+  },
+
 };
 
